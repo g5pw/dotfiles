5d27576e5112e6fcd640588bb6c6b1e7a460a7bc
Aljaž "g5pw" Srebrnič <a2piratesoft@gmail.com>
2012-06-11 23:16:25 +0200

    Added some sweet 256 color and utf8 goodness
    to vcs_info in zsh.

--- zsh/zsh.d/99_vcs_prompt.zsh
+++ zsh/zsh.d/99_vcs_prompt.zsh
@@ -2,12 +2,12 @@
 autoload -Uz vcs_info
 
 # Disable unused VCS
-zstyle ':vcs_info:*' enable git svn hg
+zstyle ':vcs_info:*' enable git svn hg bzr
 
-zstyle ':vcs_info:*' stagedstr "%F{yellow}+%f"
-zstyle ':vcs_info:*' unstagedstr "%F{green}!%f"
+zstyle ':vcs_info:*' stagedstr "%F{green}✔%f"
+zstyle ':vcs_info:*' unstagedstr "%F{red}✘%f"
 zstyle ':vcs_info:*' actionformats '%F{red}[%F{green}%b|%a%F{red}]%F{magenta}(%c%u%F{magenta})%f'
-zstyle ':vcs_info:*' formats '%F{blue}%s%f:%F{red}[%F{green}%b%F{red}]%F{magenta}(%c%u%F{magenta})%f'
+zstyle ':vcs_info:*' formats '%F{blue}%s%f:%F{yellow}%r%F{red}[%F{green}%b%F{red}]%F{magenta}(%c%u%F{magenta})%f'
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' use-prompt-escapes true
 
@@ -17,4 +17,4 @@ precmd() {
     vcs_info
     [[ -n $vcs_info_msg_0_ ]] && psvar[1]="$vcs_info_msg_0_"
 }
-export RPROMPT='%(1v.%F{red} ${vcs_info_msg_0_} %f.) %(?..[ %B%?%b ])'
+export RPROMPT='%(1v.%F{red} ${vcs_info_msg_0_} %f.)%(?.. [ %F{red}%B%?%b%f ])'
