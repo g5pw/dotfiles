0232564ec8ad8b5b7da83a0a3593fd78357fa461
Aljaž "g5pw" Srebrnič <a2piratesoft@gmail.com>
2012-06-17 13:08:41 +0200

    Small modification to the vcs display
    Added a hook to display untracked files, some aesthetic changes to the
    vcs message.

--- zsh/zsh.d/99_vcs_prompt.zsh
+++ zsh/zsh.d/99_vcs_prompt.zsh
@@ -4,17 +4,23 @@ autoload -Uz vcs_info
 # Disable unused VCS
 zstyle ':vcs_info:*' enable git svn hg bzr
 
-zstyle ':vcs_info:*' stagedstr "%F{green}✔%f"
-zstyle ':vcs_info:*' unstagedstr "%F{red}✘%f"
+zstyle ':vcs_info:*' stagedstr '%F{28}●'
+zstyle ':vcs_info:*' unstagedstr '%F{11}●'
 zstyle ':vcs_info:*' actionformats '%F{red}[%F{green}%b|%a%F{red}]%F{magenta}(%c%u%F{magenta})%f'
-zstyle ':vcs_info:*' formats '%F{blue}%s%f:%F{yellow}%r%F{red}[%F{green}%b%F{red}]%F{magenta}(%c%u%F{magenta})%f'
+zstyle ':vcs_info:*' formats '%F{blue}%s%f:%F{yellow}%r%F{red}[%F{green}%b%F{red}]%F{magenta}(%c%u%m%F{magenta})%f'
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' use-prompt-escapes true
+zstyle ':vcs_info:git+set-message:*' hooks git_unstaged_files
+
+function +vi-git_unstaged_files() {
+    if [[ -z $(git ls-files --other --exclude-standard 2> /dev/null) ]]; then;
+    	hook_com[misc]=$hook_com[misc_orig]
+    else
+	hook_com[misc]="%F{red}●$hook_com[misc_orig]"
+    fi
+}
 
 precmd() {
-    print -Pn  "\033]0;%~\007"
-    psvar=()
     vcs_info
-    [[ -n $vcs_info_msg_0_ ]] && psvar[1]="$vcs_info_msg_0_"
 }
-export RPROMPT='%(1v.%F{red} ${vcs_info_msg_0_} %f.)%(?.. [ %F{red}%B%?%b%f ])'
+export RPROMPT='%F{red}${vcs_info_msg_0_}%f%(?.. [ %F{red}%B%?%b%f ])'
