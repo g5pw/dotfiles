934018180fa5c3c8e5b8bca8373995f847e4edb6
Aljaž "g5pw" Srebrnič <a2piratesoft@gmail.com>
2013-01-08 11:13:09 +0100

    zsh: simplify VCS prompt

--- zsh/zsh.d/99_vcs_prompt.zsh
+++ zsh/zsh.d/99_vcs_prompt.zsh
@@ -4,17 +4,17 @@ autoload -Uz vcs_info
 # Disable unused VCS
 zstyle ':vcs_info:*' enable git svn hg bzr
 
-zstyle ':vcs_info:*' stagedstr '%F{28}●'
-zstyle ':vcs_info:*' unstagedstr '%F{11}●'
+zstyle ':vcs_info:*' stagedstr '%F{28}◉'
+zstyle ':vcs_info:*' unstagedstr '%F{11}⦿'
 zstyle ':vcs_info:*' actionformats '%F{red}[%F{green}%b|%a%F{red}]%F{magenta}(%c%u%m%F{magenta})%f'
-zstyle ':vcs_info:*' formats '%F{blue}%s%f:%F{yellow}%r%F{red}[%F{green}%b%F{red}]%F{magenta}(%c%u%m%F{magenta})%f'
+zstyle ':vcs_info:*' formats "$BG[123]⮀$FG[069] %s%f $BG[229]$FG[123]⮀ $FG[033]$FX[bold]%r$FX[no-bold] $BG[177]$FG[229]⮀$FG[088] ⭠ %b $FX[reset]$FG[177]⮀ %F{magenta}%c%u%m "
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' use-prompt-escapes true
 zstyle ':vcs_info:git+set-message:*' hooks git_check_unstaged_files
 
 function +vi-git_check_unstaged_files() {
     if [[ -z $(git ls-files --other --exclude-standard 2> /dev/null) ]]; then;
-    	hook_com[misc]=$hook_com[misc_orig]
+	hook_com[misc]=$hook_com[misc_orig]
     else
 	hook_com[misc]="%F{red}●$hook_com[misc_orig]"
     fi
@@ -23,4 +23,5 @@ function +vi-git_check_unstaged_files() {
 precmd() {
     vcs_info
 }
-export RPROMPT='%F{red}${vcs_info_msg_0_}%f%(?.. [ %F{red}%B%?%b%f ])'
+
+export RPROMPT=$'$FG[000]%(?..$FG[007]$BG[196]$FX[bold] ✘ %?$FX[no-bold]$FG[196]) ${vcs_info_msg_0_}$FX[reset]'
