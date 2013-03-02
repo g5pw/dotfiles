9aff9f337b88b7bc05c2e851a44cb9ba05916f13
Aljaž "g5pw" Srebrnič <a2piratesoft@gmail.com>
2013-03-02 23:09:00 +0100

    zsh: add better vcs merge prompt

--- zsh/zsh.d/99_vcs_prompt.zsh
+++ zsh/zsh.d/99_vcs_prompt.zsh
@@ -6,8 +6,8 @@ zstyle ':vcs_info:*' enable git svn hg bzr
 
 zstyle ':vcs_info:*' stagedstr '%F{28}◉'
 zstyle ':vcs_info:*' unstagedstr '%F{11}⦿'
-zstyle ':vcs_info:*' actionformats '%F{red}[%F{green}%b|%a%F{red}]%F{magenta}(%c%u%m%F{magenta})%f'
-zstyle ':vcs_info:*' formats "$BG[123]⮀$FG[069] %s%f $BG[229]$FG[123]⮀ $FG[033]$FX[bold]%r$FX[no-bold] $BG[177]$FG[229]⮀$FG[088] ⭠ %b $FX[reset]$FG[177]⮀ %F{magenta}%c%u%m "
+zstyle ':vcs_info:*' actionformats "$BG[88]⮀$FG[67]%a$BG[123]⮀$FG[069] %s $BG[229]$FG[123]⮀ $FG[033]$FX[bold]%r$FX[no-bold] $BG[177]$FG[229]⮀$FG[088] ⭠ %b $FX[reset]$FG[177]⮀ %F{magenta}%c%u%m "
+zstyle ':vcs_info:*' formats "$BG[123]⮀$FG[069] %s $BG[229]$FG[123]⮀ $FG[033]$FX[bold]%r$FX[no-bold] $BG[177]$FG[229]⮀$FG[088] ⭠ %b $FX[reset]$FG[177]⮀ %F{magenta}%c%u%m "
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' use-prompt-escapes true
 zstyle ':vcs_info:git+set-message:*' hooks git_check_unstaged_files
