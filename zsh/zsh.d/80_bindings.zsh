a0f4c5b346ee90f84d0bb09ce361dabf7f00cb7b
Aljaž "g5pw" Srebrnič <a2piratesoft@gmail.com>
2013-07-08 20:11:33 +0200

    zsh: Add fasd bindings

--- zsh/zsh.d/80_bindings.zsh
+++ zsh/zsh.d/80_bindings.zsh
@@ -38,3 +38,7 @@ bindkey "^[c" insert-caffeinate
 bindkey "^[u" undo
 
 bindkey -M vicmd v edit-command-line
+
+bindkey '^X^A' fasd-complete    # C-x C-a to do fasd-complete (fils and directories)
+bindkey '^X^F' fasd-complete-f  # C-x C-f to do fasd-complete-f (only files)
+bindkey '^X^D' fasd-complete-d  # C-x C-d to do fasd-complete-d (only directories))
