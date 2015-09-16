fe01ce336255942edafe05c2221090dd7b6fa8b2
Aljaž "g5pw" Srebrnič <a2piratesoft@gmail.com>
2015-09-16 14:21:51 +0200

    zsh: Fix unicode chars in prompt

--- zsh/zsh.d/90_prompt.zsh
+++ zsh/zsh.d/90_prompt.zsh
@@ -2,11 +2,11 @@
 zstyle  ':prompt:rtab' fish yes
 mode_color=178
 battery='$BG[239]$(bat_status)$FG[239] '
-user_host='$BG[60]⮀ $FG[7]$FX[bold]%n $FX[no-bold]$FG[60]'
-dir='$BG[75]⮀ $FG[0]$(rtab)$FG[75] '
-clock='$BG[204]⮀$FG[228]%* '
-export PROMPT=$battery$user_host$dir$clock'$FX[reset]$FG[204]⮀ $FG[$mode_color]%#$FX[reset] '
-export PROMPT2='$FG[131]%_$FG[121] ⮀ $FX[reset]'
+user_host='$BG[60] $FG[7]$FX[bold]%n$FX[no-bold]@%m:%l $FG[60]'
+dir='$BG[75] $FG[0]$(rtab)$FG[75] '
+clock='$BG[204]$FG[228]%T '
+export PROMPT=$user_host$dir$clock'$FX[reset]$FG[204] $FG[$mode_color]%#$FX[reset] '
+export PROMPT2='$FG[131]%_$FG[121]  $FX[reset]'
 
 ## ask me 'do you wish to see all XX possibilities' before menu selection
 export LISTPROMPT=''
--- zsh/zsh.d/99_vcs_prompt.zsh
+++ zsh/zsh.d/99_vcs_prompt.zsh
@@ -9,8 +9,8 @@ zstyle ':vcs_info:git:*:-all-' command $(whence -p git)
 
 zstyle ':vcs_info:*' stagedstr '%F{28}◉'
 zstyle ':vcs_info:*' unstagedstr '%F{11}⦿'
-zstyle ':vcs_info:*' actionformats "$BG[88]⮀$FG[67]%a$BG[123]⮀$FG[69] %s $BG[229]$FG[123]⮀ $FG[33]$FX[bold]%r$FX[no-bold] $BG[177]$FG[229]⮀$FG[88] ⭠ %b $FX[reset]$FG[177]⮀ %F{magenta}%c%u%m "
-zstyle ':vcs_info:*' formats "$BG[123]⮀$FG[69] %s $BG[229]$FG[123]⮀ $FG[33]$FX[bold]%r$FX[no-bold] $BG[177]$FG[229]⮀$FG[88] ⭠ %b $FX[reset]$FG[177]⮀ %F{magenta}%c%u%m "
+zstyle ':vcs_info:*' actionformats "$BG[88]$FG[67]%a$BG[123]$FG[69] %s $BG[229]$FG[123] $FG[33]$FX[bold]%r$FX[no-bold] $BG[177]$FG[229]$FG[88]  %b $FX[reset]$FG[177] %F{magenta}%c%u%m "
+zstyle ':vcs_info:*' formats "$BG[123]$FG[69] %s $BG[229]$FG[123] $FG[33]$FX[bold]%r$FX[no-bold] $BG[177]$FG[229]$FG[88]  %b $FX[reset]$FG[177] %F{magenta}%c%u%m "
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' use-prompt-escapes true
 zstyle ':vcs_info:git+set-message:*' hooks git_check_unstaged_files
