c54ef06073921a202c1ae76dba912a706bf967a1
Aljaž "g5pw" Srebrnič <a2piratesoft@gmail.com>
2018-01-24 11:51:23 +0100

    zsh: configured spaceship prompt

--- zsh/zsh.d/90_prompt.zsh
+++ zsh/zsh.d/90_prompt.zsh
@@ -1,33 +1,12 @@
-## set prompts ####
-zstyle  ':prompt:rtab' fish yes
-mode_color=178
-battery='$BG[239]$(bat_status)$FG[239] '
-user_host='$FG[236]$BG[141] $FG[16]$FX[bold]%n$FX[no-bold]@%m:%l $FG[141]'
-dir='$BG[61] $FG[15]$(rtab)$FG[61] '
-clock='$BG[6]$FG[236]%T '
-export PROMPT=$user_host$dir$clock'$FX[reset]$FG[6] $FG[$mode_color]%#$FX[reset] '
-export PROMPT2='$FG[131]%_$FG[121]  $FX[reset]'
+autoload -U promptinit; promptinit
 
-## ask me 'do you wish to see all XX possibilities' before menu selection
-export LISTPROMPT=''
+prompt spaceship
 
-## SPROMPT - the spelling prompt
-export SPROMPT='zsh: correct %F{red}'%R'%f to %F{green}'%r'%f? (%F{green}[Y]%fes/%F{red}[N]%fo/%F{yellow}[E]%fdit/%F{cyan}[A]%fbort) '
-
-export ZCALCPROMPT="$FX[bold]$FG[208]%1v$FX[no-bold]$FG[111] ⫸ $FX[reset] "
+SPACESHIP_PROMPT_ADD_NEWLINE=false
+SPACESHIP_PROMPT_SEPARATE_LINE=false
 
 function zle-keymap-select {
-    if [[ $KEYMAP == vicmd ]]; then
-      mode_color=37
-    else
-      mode_color=178
-    fi
     zle reset-prompt
 }
 
-function zle-line-finish {
-    mode_color=178
-}
-
 zle -N zle-keymap-select
-zle -N zle-line-finish
