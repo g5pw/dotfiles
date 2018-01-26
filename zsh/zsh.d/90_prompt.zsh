a5359572b56d54338af308ef9483df3456774ab7
Aljaž "g5pw" Srebrnič <a2piratesoft@gmail.com>
2018-01-26 09:58:39 +0100

    zsh: restore other prompts

--- zsh/zsh.d/90_prompt.zsh
+++ zsh/zsh.d/90_prompt.zsh
@@ -5,6 +5,14 @@ prompt spaceship
 SPACESHIP_PROMPT_ADD_NEWLINE=false
 SPACESHIP_PROMPT_SEPARATE_LINE=false
 
+## ask me 'do you wish to see all XX possibilities' before menu selection
+export LISTPROMPT=''
+
+## SPROMPT - the spelling prompt
+export SPROMPT='zsh: correct %F{red}'%R'%f to %F{green}'%r'%f? (%F{green}[Y]%fes/%F{red}[N]%fo/%F{yellow}[E]%fdit/%F{cyan}[A]%fbort) '
+
+export ZCALCPROMPT="$FX[bold]$FG[208]%1v$FX[no-bold]$FG[111] ⫸ $FX[reset] "
+
 function zle-keymap-select {
     zle reset-prompt
 }
