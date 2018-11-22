05340093fc63b66f3e4d5fbd5c63a22f6523cb62
Aljaž Srebrnič <aljaz.srebrnic@mquadro.net>
2018-11-22 11:13:40 +0100

    zsh: automagically use `fd` if available

--- zsh/zsh.d/99_fzf.zsh
+++ zsh/zsh.d/99_fzf.zsh
@@ -3,3 +3,10 @@ source /usr/share/fzf/shell/key-bindings.zsh
 alias p="fzf --preview 'bat --color \"always\" {}'"
 # add support for ctrl+o to open selected file in VS Code
 export FZF_DEFAULT_OPTS="--bind='ctrl-o:execute(emacsclient -nw {})+abort'"
+
+if which fd &>/dev/null; then
+    # Prefer using `fd`
+    export FZF_DEFAULT_COMMAND='fd --type file --color=always'
+    export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --ansi"
+    export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+fi
