32ae4e1ab39cf44a4096e47dae8c8fc062f9085b
Aljaž Srebrnič <aljaz.srebrnic@mquadro.net>
2018-12-20 12:27:06 +0100

    zsh: enhance fzf with fd

--- zsh/zsh.d/99_fzf.zsh
+++ zsh/zsh.d/99_fzf.zsh
@@ -1,12 +1,32 @@
 source /usr/share/fzf/shell/key-bindings.zsh
 
-alias p="fzf --preview 'bat --color \"always\" {}'"
+alias pw="fzf --preview 'bat --color \"always\" {}'"
+
 # add support for ctrl+o to open selected file in VS Code
 export FZF_DEFAULT_OPTS="--bind='ctrl-o:execute(emacsclient -nw {})+abort'"
 
+# Use ~~ as the trigger sequence instead of the default **
+export FZF_COMPLETION_TRIGGER='~~'
+
+# Options to fzf command
+export FZF_COMPLETION_OPTS='+c -x'
+
+export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
+
 if which fd &>/dev/null; then
-    # Prefer using `fd`
-    export FZF_DEFAULT_COMMAND='fd --type file --color=always'
-    export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --ansi"
+    # Use fd (https://github.com/sharkdp/fd) instead of the default find
+    # command for listing path candidates.
+    # - The first argument to the function ($1) is the base path to start traversal
+    # - See the source code (completion.{bash,zsh}) for the details.
+    _fzf_compgen_path() {
+	fd --hidden --follow --exclude ".git" . "$1"
+    }
+
+    # Use fd to generate the list for directory completion
+    _fzf_compgen_dir() {
+	fd --type d --hidden --follow --exclude ".git" . "$1"
+    }
+
+    export FZF_DEFAULT_COMMAND='fd --type file'
     export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 fi
