6cd4bdc30333611420833ac3876f4e9568b28286
Aljaž Srebrnič <aljaz.srebrnic@mquadro.net>
2019-05-29 09:41:01 +0200

    zsh: cap maximum autosuggestions to 30 chars

--- zsh/zsh.d/91_autosuggestions.zsh
+++ zsh/zsh.d/91_autosuggestions.zsh
@@ -1,9 +1,11 @@
 # Load zsh-autosuggestions
 [ -d $ZDOTDIR/zsh-autosuggestions ] || exit 255
 
-source $ZDOTDIR/zsh-autosuggestions/zsh-autosuggestions.zsh
 export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=242'
 export ZSH_AUTOSUGGEST_USE_ASYNC='yes'
+export ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=30
+
+source $ZDOTDIR/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 # Bindings
 bindkey "^o" autosuggest-execute
