457efa80a48c947b2a74ce2954396d08c9351f1b
Aljaž "g5pw" Srebrnič <a2piratesoft@gmail.com>
2019-07-11 23:41:37 +0200

    zsh: enable fast-zsh-highlight

--- zsh/zsh.d/91_syntax_highlighting.zsh
+++ zsh/zsh.d/91_syntax_highlighting.zsh
@@ -1,4 +1,4 @@
 # load highlighting
 [ -d $ZDOTDIR/zsh-syntax-highlighting/ ] || exit 255
 
-source $ZDOTDIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source $ZDOTDIR/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
