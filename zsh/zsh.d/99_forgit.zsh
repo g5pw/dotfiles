adfc2c8400fcbfcd443a209483b56c6c7bae0324
Aljaž Srebrnič <aljaz.srebrnic@mquadro.net>
2019-07-26 13:18:33 +0200

    zsh: add forgit preview keybinds

--- zsh/zsh.d/99_forgit.zsh
+++ zsh/zsh.d/99_forgit.zsh
@@ -2,4 +2,8 @@ forgit_log=gli
 forgit_diff=gdi
 forgit_add=gai
 forgit_stash_show=gssi
+FZF_DEFAULT_OPTS="
+--bind='ctrl-f:preview-page-down,ctrl-e:preview-down'
+--bind='ctrl-b:preview-page-up,ctrl-y:preview-up'
+"
 source $ZDOTDIR/forgit/forgit.plugin.zsh
