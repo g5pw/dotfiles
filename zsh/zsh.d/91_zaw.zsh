b3893673a49bc56aa7f73c64d16872d3e04959fd
Aljaž Srebrnič <aljaz.srebrnic@mquadro.net>
2019-08-07 14:34:06 +0200

    zsh:zaw: add some sensible keybinds, tweak colors

--- zsh/zsh.d/91_zaw.zsh
+++ zsh/zsh.d/91_zaw.zsh
@@ -3,7 +3,11 @@ zplugin load 'zsh-users/zaw'
 
 zstyle ':filter-select:highlight' matched   fg=yellow,standout
 zstyle ':filter-select:highlight' title     fg=cyan,underline
+zstyle ':filter-select:highlight' selected  bg=240,bold
 zstyle ':filter-select' max-lines -10       # use $LINES - 10 for filter-select
 zstyle ':filter-select' rotate-list yes     # enable rotation for filter-select
 zstyle ':filter-select' case-insensitive yes
 zstyle ':filter-select' extended-search yes
+
+bindkey -M filterselect '\e' send-break
+bindkey -M filterselect '^d' send-break
