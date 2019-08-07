da473321095608fed7639c902c2f401b293c3544
Aljaž Srebrnič <aljaz.srebrnic@mquadro.net>
2019-08-07 14:32:48 +0200

    zsh:forgit: add exports

--- zsh/zsh.d/99_forgit.zsh
+++ zsh/zsh.d/99_forgit.zsh
@@ -1,8 +1,9 @@
-forgit_log=gli
-forgit_diff=gdi
-forgit_add=gai
-forgit_stash_show=gssi
-FZF_DEFAULT_OPTS="
+export forgit_log=gli
+export forgit_diff=gdi
+export forgit_add=gai
+export forgit_stash_show=gssi
+export FORGIT_COPY_CMD="xclip -i -selection clipboard"
+export FZF_DEFAULT_OPTS="
 --bind='ctrl-f:preview-page-down,ctrl-e:preview-down'
 --bind='ctrl-b:preview-page-up,ctrl-y:preview-up'
 "
