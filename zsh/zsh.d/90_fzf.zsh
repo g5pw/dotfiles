8c4cb86624e1ca577e9dfcfe6a56f9462048fdfa
Aljaž "g5pw" Srebrnič <a2piratesoft@gmail.com>
2019-10-15 10:57:41 +0200

    zsh: remove local fzf

--- zsh/zsh.d/90_fzf.zsh
+++ zsh/zsh.d/90_fzf.zsh
@@ -1,5 +1,3 @@
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-
 alias pw="fzf --preview 'bat --color \"always\" {}'"
 # add support for ctrl+o to open selected file in VS Code
 export FZF_DEFAULT_OPTS="--bind='ctrl-o:execute(emacsclient -nw {})+abort'"
