310cba06a0b1a2c1957911d0f60789349565a8e7
Aljaž "g5pw" Srebrnič <a2piratesoft@gmail.com>
2019-11-29 09:39:41 +0100

    emacs: update/port config from spacemacs

--- doom.d/config.el
+++ doom.d/config.el
@@ -1,5 +1,89 @@
 ;;; .doom.d/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here
-
 (setq doom-font (font-spec :family "Iosevka" :size 13))
+
+(setq ibuffer-group-buffers-by "projects")
+
+(setq ispell-dictionary "italiano")
+
+;; Lets me type special combining characters
+(setq mac-right-option-modifier nil)
+
+(setq-default TeX-master nil)
+(setq TeX-global-PDF-mode t
+      TeX-auto-save t
+      TeX-parse-self t
+      TeX-view-program-list '(("Skim" "/usr/bin/open -a Skim.app %o"))
+      TeX-view-program-selection '((output-pdf "Skim"))
+      TeX-source-correlate-mode t)
+
+(add-hook! org-mode
+           '(add-to-list 'org-modules 'org-habit)
+           '(setcdr (assoc "\\.pdf\\'" org-file-apps) "open -a Skim %s"))
+
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("/views/.*\\.php\\'" . web-mode))
+
+;; Set keys
+;;(spacemacs/set-leader-keys-for-major-mode 'org-mode "v" 'org-toggle-latex-fragment)
+;;(spacemacs/set-leader-keys-for-major-mode 'org-mode "S" 'org-sidebar)
+;;(spacemacs/set-leader-keys-for-minor-mode 'org-beamer-mode "ep" 'org-beamer-export-to-pdf)
+;;(spacemacs/set-leader-keys-for-minor-mode 'org-beamer-mode "eo" '(lambda () (interactive) (org-open-file (org-beamer-export-to-pdf))))
+;;(spacemacs/set-leader-keys-for-minor-mode 'org-beamer-mode "se" 'org-beamer-select-environment)
+
+(setq calendar-week-start-day 1)
+
+(setq org-cycle-separator-lines 0
+      org-catch-invisible-edits 'show-and-error
+      org-list-indent-offset 1
+      org-agenda-start-on-weekday 1
+      org-bullets-bullet-list '("■" "◆" "✦" "✸" "✱")
+      org-ellipsis "↴"
+      org-return-follows-link t
+      org-enforce-todo-dependencies t
+      org-enforce-todo-checkbox-dependencies t
+      org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "STARTED(s)" "WAITING(w)"
+                                    "|"
+                                    "DONE(d)" "DELEGATED(l)" "CANCELED(c)"))
+      org-latex-create-formula-image-program 'dvipng
+      org-latex-packages-alist '(("" "siunitx" t))
+      org-refile-targets (quote ((nil :maxlevel . 2)
+                                 (org-agenda-files :maxlevel . 2))))
+
+(setq org-super-agenda-groups
+      '((:name "Next actions"  ; Optionally specify section name
+               :time-grid t  ; Items that appear on the time grid
+               :todo "NEXT")  ; Items that have this TODO keyword
+        (:name "test"
+               :deadline "today")
+        (:name "Important"
+               :priority "A")))
+
+(define-key evil-normal-state-map "~" 'evil-invert-case)
+(define-key evil-visual-state-map "~" 'evil-invert-case)
+
+(unless (display-graphic-p)
+  (require 'evil-terminal-cursor-changer)
+  (evil-terminal-cursor-changer-activate)
+  )
+
+(define-key evil-normal-state-map (kbd "C-a") 'evil-numbers/inc-at-pt)
+(define-key evil-visual-state-map (kbd "C-a") 'evil-numbers/inc-at-pt)
+(define-key evil-normal-state-map (kbd "C-x") 'evil-numbers/dec-at-pt)
+(define-key evil-visual-state-map (kbd "C-x") 'evil-numbers/dec-at-pt)
+
+(tool-bar-mode -1)
+
+;; Set normal emacs window size
+(if (display-graphic-p)
+    (setq default-frame-alist
+          '((width . 140)
+            (height . 40))))
+
+(after! forge
+  '(add-to-list 'forge-alist
+                '("git-ssh.mittelab.org" "git.mittelab.org/api/v4" "git.mittelab.org" forge-gitlab-repository)))
+
+(load-file "~/.emacs.private.el")
--- doom.d/init.el
+++ doom.d/init.el
@@ -16,27 +16,30 @@
        company           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       ivy               ; a search engine for love and life
+       (ivy                ; a search engine for love and life
+        +prescient
+        +fuzzy
+        +icons)
 
        :ui
        ;;deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
-       doom-quit         ; DOOM quit-message prompts when you quit Emacs
+       ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
        ;;fill-column       ; a `fill-column' indicator
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
        ;;indent-guides     ; highlighted indent columns
        modeline          ; snazzy, Atom-inspired modeline, plus API
-       nav-flash         ; blink the current line after jumping
+       ;;nav-flash         ; blink the current line after jumping
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
        (popup            ; tame sudden yet inevitable temporary windows
         +all             ; catch all popups that start with an asterix
         +defaults)       ; default popup rules
-       ;;pretty-code       ; replace bits of code with pretty symbols
+       pretty-code       ; replace bits of code with pretty symbols
        ;;tabs              ; an tab bar for Emacs
-       ;;treemacs          ; a project drawer, like neotree but cooler
+       treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
@@ -64,40 +67,40 @@
        vc                ; version-control and Emacs, sitting in a tree
 
        :term
-       ;;eshell            ; a consistent, cross-platform shell (WIP)
+       eshell            ; a consistent, cross-platform shell (WIP)
        ;;shell             ; a terminal REPL for Emacs
        ;;term              ; terminals in Emacs
        ;;vterm             ; another terminals in Emacs
 
        :tools
-       ;;ansible
+       ansible
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
-       ;;docker
-       ;;editorconfig      ; let someone else argue about tabs vs spaces
-       ;;ein               ; tame Jupyter notebooks with emacs
+       docker
+       editorconfig      ; let someone else argue about tabs vs spaces
+       ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        flycheck          ; tasing you for every semicolon you forget
-       ;;flyspell          ; tasing you for misspelling mispelling
+       flyspell          ; tasing you for misspelling mispelling
        ;;gist              ; interacting with github gists
        (lookup           ; helps you navigate your code and documentation
         +docsets)        ; ...or in Dash docsets locally
-       ;;lsp
-       ;;macos             ; MacOS-specific commands
+       lsp
+       macos             ; MacOS-specific commands
        magit             ; a git porcelain for Emacs
-       ;;make              ; run make tasks from Emacs
+       make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
        ;;pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
-       ;;rgb               ; creating color strings
+       rgb               ; creating color strings
        ;;terraform         ; infrastructure as code
-       ;;tmux              ; an API for interacting with tmux
+       tmux              ; an API for interacting with tmux
        ;;upload            ; map local to remote projects via ssh/ftp
 
        :lang
        ;;agda              ; types of types of types of types...
        ;;assembly          ; assembly for fun or debugging
-       ;;cc                ; C/C++/Obj-C madness
+       cc                ; C/C++/Obj-C madness
        ;;clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
@@ -116,10 +119,10 @@
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ;
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
-       ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
+       javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
-       ;;latex             ; writing papers in Emacs has never been so fun
+       latex             ; writing papers in Emacs has never been so fun
        ;;lean
        ;;ledger            ; an accounting system in Emacs
        ;;lua               ; one-based indices? one-based indices
@@ -135,16 +138,16 @@
         ;;+pomodoro        ; be fruitful with the tomato technique
         +present)        ; using org-mode for presentations
        ;;perl              ; write code no one else can comprehend
-       ;;php               ; perl's insecure younger brother
-       ;;plantuml          ; diagrams for confusing people more
+       php               ; perl's insecure younger brother
+       plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       ;;python            ; beautiful is better than ugly
+       python            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
-       ;;rest              ; Emacs as a REST client
+       rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
        ;;ruby              ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;scheme            ; a fully conniving family of lisps
        sh                ; she sells {ba,z,fi}sh shells on the C xor
