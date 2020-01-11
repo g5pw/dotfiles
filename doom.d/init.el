dcdb809f05653ae2df0874efb29efc83dbb687ee
Aljaž "g5pw" Srebrnič <a2piratesoft@gmail.com>
2020-01-11 11:32:01 +0100

    :emacs: update doom config
    flyspell/check moved to checkers

--- doom.d/init.el
+++ doom.d/init.el
@@ -12,6 +12,11 @@
        ;;chinese
        ;;japanese
 
+       :checkers
+       (spell
+        +aspell)
+       syntax
+
        :completion
        company           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
@@ -82,8 +87,6 @@
        editorconfig      ; let someone else argue about tabs vs spaces
        ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
-       flycheck          ; tasing you for every semicolon you forget
-       flyspell          ; tasing you for misspelling mispelling
        ;;gist              ; interacting with github gists
        (lookup           ; helps you navigate your code and documentation
         +docsets)        ; ...or in Dash docsets locally
