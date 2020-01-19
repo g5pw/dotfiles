ce9642301682b741348d8bb92abbdaf0208d69f1
Aljaž "g5pw" Srebrnič <a2piratesoft@gmail.com>
2020-01-19 11:35:39 +0100

    emacs: enable pdf and upload support

--- doom.d/init.el
+++ doom.d/init.el
@@ -96,12 +96,12 @@
        magit             ; a git porcelain for Emacs
        make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
-       ;;pdf               ; pdf enhancements
+       pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        rgb               ; creating color strings
        ;;terraform         ; infrastructure as code
        tmux              ; an API for interacting with tmux
-       ;;upload            ; map local to remote projects via ssh/ftp
+       upload            ; map local to remote projects via ssh/ftp
 
        :lang
        ;;agda              ; types of types of types of types...
