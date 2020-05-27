b0ff17206dd194a31cee2ad1687fd3d5420d5b0b
Aljaž "g5pw" Srebrnič <a2piratesoft@gmail.com>
2020-05-27 22:08:15 +0200

    zsh: fixup mac aliases

--- tag-mac/zsh/zsh.d/31_aliases.zsh
+++ tag-mac/zsh/zsh.d/31_aliases.zsh
@@ -1,9 +1,6 @@
-alias airport='/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport'
-alias code='/Applications/VSCodium.app/Contents/Resources/app/bin/code'
 alias llusb="system_profiler SPUSBDataType"
+alias airport='/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport'
 alias lswifi='/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -s | sed 1d | sort -rnk 3'
-if which port &> /dev/null; then
-    alias psy="sudo port sync"
-    alias pi='portindex'
-    alias p='noglob port-wrapper'
-fi
+alias psy="sudo port sync"
+alias pi='portindex'
+alias p='noglob port-wrapper'
