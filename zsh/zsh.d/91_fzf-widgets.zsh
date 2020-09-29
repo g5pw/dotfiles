579a3b344b7071d58c00b626497ad48b6039da86
Aljaž Srebrnič <aljaz.srebrnic@mquadro.net>
2020-09-29 16:25:59 +0200

    zsh: reorganize fzf, add preview for branch

--- zsh/zsh.d/91_fzf-widgets.zsh
+++ zsh/zsh.d/91_fzf-widgets.zsh
@@ -3,38 +3,10 @@ zinit load ytet5uy4/fzf-widgets
 autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
 add-zsh-hook chpwd chpwd_recent_dirs
 
-local fzf_widgets_prefix="^e"
-
-# Map widgets to key
-bindkey "${fzf_widgets_prefix}"  fzf-select-widget
-bindkey "${fzf_widgets_prefix}c" fzf-change-directory
-bindkey "${fzf_widgets_prefix}n" fzf-change-named-directory
-bindkey "${fzf_widgets_prefix}f" fzf-edit-files
-bindkey "${fzf_widgets_prefix}k" fzf-kill-processes
-bindkey "${fzf_widgets_prefix}s" fzf-exec-ssh
-bindkey "^\\"  fzf-change-recent-directory
-bindkey "^xf" fzf-insert-files
-bindkey "^xd" fzf-insert-directory
-bindkey "^xn" fzf-insert-named-directory
-
-## Git
-bindkey "${fzf_widgets_prefix}g"  fzf-select-git-widget
-bindkey "${fzf_widgets_prefix}ga" fzf-git-add-files
-bindkey "${fzf_widgets_prefix}gc" fzf-git-change-repository
-
-# GitHub
-bindkey "${fzf_widgets_prefix}h"  fzf-select-github-widget
-bindkey "${fzf_widgets_prefix}hs" fzf-github-show-issue
-bindkey "${fzf_widgets_prefix}hc" fzf-github-close-issue
-
-## Docker
-bindkey "${fzf_widgets_prefix}d"  fzf-select-docker-widget
-bindkey "${fzf_widgets_prefix}dc" fzf-docker-remove-containers
-bindkey "${fzf_widgets_prefix}di" fzf-docker-remove-images
-bindkey "${fzf_widgets_prefix}dv" fzf-docker-remove-volumes
-
 # Enable Exact-match by fzf-insert-history
 FZF_WIDGET_OPTS[insert-history]='--exact'
+FZF_WIDGET_OPTS[git-checkout-branch]="--preview=git log --oneline --graph --color --decorate=short {1}"
+FZF_WIDGET_OPTS[git-delete-branch]="--preview=git log --oneline --graph --color --decorate=short {1}"
 
 
 fzf-insert-git-sha() {
@@ -55,6 +27,7 @@ fzf-insert-git-sha() {
     cut -d' ' -f 2 | \
     __fzf::widget::insert
 }
+zle -N fzf-insert-git-sha
 
 fzf-fixup-commit() {
     if ! git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
@@ -76,8 +49,38 @@ fzf-fixup-commit() {
 
     __fzf::widget::exec
 }
-
-zle -N fzf-insert-git-sha
 zle -N fzf-fixup-commit
+
+local fzf_widgets_prefix="^e"
+
+# Map widgets to key
+bindkey "${fzf_widgets_prefix}"  fzf-select-widget
+bindkey "${fzf_widgets_prefix}c" fzf-change-directory
+bindkey "${fzf_widgets_prefix}n" fzf-change-named-directory
+bindkey "${fzf_widgets_prefix}f" fzf-edit-files
+bindkey "${fzf_widgets_prefix}k" fzf-kill-processes
+bindkey "${fzf_widgets_prefix}s" fzf-exec-ssh
+bindkey "^\\"  fzf-change-recent-directory
+bindkey "^xf" fzf-insert-files
+bindkey "^xd" fzf-insert-directory
+bindkey "^xn" fzf-insert-named-directory
+
+## Git
+bindkey "${fzf_widgets_prefix}g"  fzf-select-git-widget
+bindkey "${fzf_widgets_prefix}ga" fzf-git-add-files
+bindkey "${fzf_widgets_prefix}gc" fzf-git-change-repository
 bindkey "${fzf_widgets_prefix}gl"  fzf-insert-git-sha
 bindkey "${fzf_widgets_prefix}gf"  fzf-fixup-commit
+bindkey "${fzf_widgets_prefix}b"  fzf-git-checkout-branch
+
+# GitHub
+bindkey "${fzf_widgets_prefix}h"  fzf-select-github-widget
+bindkey "${fzf_widgets_prefix}hs" fzf-github-show-issue
+bindkey "${fzf_widgets_prefix}hc" fzf-github-close-issue
+
+## Docker
+bindkey "${fzf_widgets_prefix}d"  fzf-select-docker-widget
+bindkey "${fzf_widgets_prefix}dc" fzf-docker-remove-containers
+bindkey "${fzf_widgets_prefix}di" fzf-docker-remove-images
+bindkey "${fzf_widgets_prefix}dv" fzf-docker-remove-volumes
+
