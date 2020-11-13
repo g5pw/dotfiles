9aa9cfa082f4089c4b4e94acba231b4253d3fe42
Aljaž Srebrnič <aljaz.srebrnic@mquadro.net>
2020-11-13 09:16:46 +0100

    zsh: use for syntax, add lazydocker

--- zsh/zsh.d/50_utils.zsh
+++ zsh/zsh.d/50_utils.zsh
@@ -1,8 +1,5 @@
-zinit ice from"gh-r" as"program" mv"rust-analyzer* -> rust-analyzer"
-zinit light rust-analyzer/rust-analyzer
-
-zinit ice from"gh-r" as"program" pick"mdcat*/mdcat"
-zinit light lunaryorn/mdcat
-
-zinit ice from"gh-r" as"program"
-zinit light denisidoro/navi
+zinit wait"2" from"gh-r" as"program" lucid for \
+  denisidoro/navi \
+  jesseduffield/lazydocker \
+  pick'mdcat*/mdcat' lunaryorn/mdcat \
+  mv'rust-analyzer* -> rust-analyzer' rust-analyzer/rust-analyzer
