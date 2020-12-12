c4b692334779387d094d77b4bd5dc1c0212c1d07
Aljaž "g5pw" Srebrnič <me@g5pw.me>
2020-12-12 13:13:52 +0100

    zsh: add dog util

--- zsh/zsh.d/50_utils.zsh
+++ zsh/zsh.d/50_utils.zsh
@@ -2,4 +2,5 @@ zinit wait"2" from"gh-r" as"program" lucid for \
   denisidoro/navi \
   jesseduffield/lazydocker \
   pick'mdcat*/mdcat' lunaryorn/mdcat \
-  mv'rust-analyzer* -> rust-analyzer' rust-analyzer/rust-analyzer
+  mv'rust-analyzer* -> rust-analyzer' rust-analyzer/rust-analyzer \
+  pick"bin/dog" ogham/dog
