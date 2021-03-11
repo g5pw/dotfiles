934bfc294e1ba4b5aa6b082aa8c495b6df575225
Aljaž "g5pw" Srebrnič <me@g5pw.me>
2021-03-11 23:28:11 +0100

    zsh: add some utils

--- zsh/zsh.d/50_utils.zsh
+++ zsh/zsh.d/50_utils.zsh
@@ -4,4 +4,6 @@ zinit wait"2" from"gh-r" as"program" lucid for \
   pick'mdcat*/mdcat' lunaryorn/mdcat \
   mv'rust-analyzer* -> rust-analyzer' rust-analyzer/rust-analyzer \
   pick"bin/dog" ogham/dog \
+  mv'dry-* -> dry' moncho/dry \
+  bcicen/ctop \
   from"gh" mrowa44/emojify
