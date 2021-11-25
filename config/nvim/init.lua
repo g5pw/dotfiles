38bd4b4ef126011ed9826cc67e8447e4cad57570
Aljaž "g5pw" Srebrnič <me@g5pw.me>
2021-11-25 08:00:35 +0100

    nvim: retain old behaviour
    - should open filer in buffer current dir

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -139,7 +139,7 @@ vim.api.nvim_set_keymap('n', '<leader>sd', [[<cmd>lua require('telescope.builtin
 vim.api.nvim_set_keymap('n', '<leader>sp', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>so', [[<cmd>lua require('telescope.builtin').tags{ only_current_buffer = true }<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>?', [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '-', [[<cmd> lua require('telescope.builtin').file_browser()<CR>]], {noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '-', [[<cmd>lua require('telescope.builtin').file_browser {cwd = vim.fn.expand("%:p:h")}<CR>]], {noremap = true, silent = true })
 
 -- Treesitter configuration
 -- Parsers must be installed manually via :TSInstall
