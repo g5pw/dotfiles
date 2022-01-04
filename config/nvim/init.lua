68683889e1797c9ca25a0a0cda2e0e837d68422a
Aljaž "g5pw" Srebrnič <me@g5pw.me>
2022-01-05 00:13:57 +0100

    nvim: add mode map for lightline

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -80,6 +80,19 @@ vim.g.lightline = {
   colorscheme = 'onedark',
   active = { left = { { 'mode', 'paste' }, { 'gitbranch', 'readonly', 'filename', 'modified' } } },
   component_function = { gitbranch = 'fugitive#head' },
+  mode_map = {
+	n = 'N',
+	i = 'I',
+	R = 'R',
+	v = 'V',
+	V = 'VL',
+	["<C-v>"] = 'VB',
+	c = 'C',
+	s = 'S',
+	S = 'SL',
+	["<C-s>"] = 'SB',
+	t= 'T',
+  },
 }
 
 --Remap space as leader key
