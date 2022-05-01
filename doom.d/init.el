5e97aa4f331ea2374ca4429bb6c2c7f58b21f6ea
Aljaž "g5pw" Srebrnič <me@g5pw.me>
2022-05-01 23:34:44 +0200

    emacs: move to vertico instead of ivy

--- doom.d/init.el
+++ doom.d/init.el
@@ -25,9 +25,8 @@
         +childframe)
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       (ivy                ; a search engine for love and life
-        +prescient
-        +fuzzy
+       ;;ivy               ; a search engine for love and life
+       (vertico           ; the search engine of the future
         +icons)
 
        :ui
