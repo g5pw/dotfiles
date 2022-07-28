205657edc6162d5b9c98984ef655a40fd3529130
Aljaž Srebrnič <aljaz.srebrnic@mquadro.net>
2022-07-28 10:08:36 +0200

    zsh: update scmpuff version

--- zsh/zsh.d/25_scmpuff.zsh
+++ zsh/zsh.d/25_scmpuff.zsh
@@ -1,7 +1,7 @@
 zinit ice from"gh-r" as"program" \
 	atload'eval "$(scmpuff init -s -a=false)"' \
 	bpick"scmpuff_*linux_x64*" \
-	ver'v0.4.0'
+	ver'v0.5.0'
 zinit light mroth/scmpuff
 
 alias gs="scmpuff_status"
