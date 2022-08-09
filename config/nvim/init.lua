b1de790eb45f80a9a36c8d337805c7ef4597927a
Aljaž Srebrnič <aljaz.srebrnic@mquadro.net>
2022-08-09 15:42:42 +0200

    nvim: move config to config block

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -23,14 +23,42 @@ require('packer').startup(function()
   -- Operators
   use 'machakann/vim-sandwich' -- bigger, better vim-surround
   -- UI to select things (files, grep results, open buffers...)
-  use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+  use { 
+      'nvim-telescope/telescope.nvim',
+      requires = { 'nvim-lua/plenary.nvim' },
+      config = function()
+          require('telescope').setup {
+              defaults = {
+                  mappings = {
+                      i = {
+                          ['<C-u>'] = false,
+                          ['<C-d>'] = false,
+                      },
+                  },
+              },
+          }
+      end
+  }
   use { "nvim-telescope/telescope-file-browser.nvim" }
   use 'joshdick/onedark.vim' -- Theme inspired by Atom
   use 'itchyny/lightline.vim' -- Fancier statusline
   -- Add indentation guides even on blank lines
   use 'lukas-reineke/indent-blankline.nvim'
   -- Add git related info in the signs columns and popups
-  use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+  use { 'lewis6991/gitsigns.nvim',
+  requires = { 'nvim-lua/plenary.nvim' },
+  config = function()
+      require('gitsigns').setup {
+          signs = {
+              add = { hl = 'GitGutterAdd', text = '+' },
+              change = { hl = 'GitGutterChange', text = '~' },
+              delete = { hl = 'GitGutterDelete', text = '_' },
+              topdelete = { hl = 'GitGutterDelete', text = '‾' },
+              changedelete = { hl = 'GitGutterChange', text = '~' },
+          },
+      }
+  end
+  }
   -- Highlight, edit, and navigate code using a fast incremental parsing library
   use 'nvim-treesitter/nvim-treesitter'
   -- Additional textobjects for treesitter
@@ -134,29 +162,6 @@ vim.g.indent_blankline_filetype_exclude = { 'help', 'packer' }
 vim.g.indent_blankline_buftype_exclude = { 'terminal', 'nofile' }
 vim.g.indent_blankline_char_highlight = 'LineNr'
 vim.g.indent_blankline_show_trailing_blankline_indent = false
-
--- Gitsigns
-require('gitsigns').setup {
-  signs = {
-    add = { hl = 'GitGutterAdd', text = '+' },
-    change = { hl = 'GitGutterChange', text = '~' },
-    delete = { hl = 'GitGutterDelete', text = '_' },
-    topdelete = { hl = 'GitGutterDelete', text = '‾' },
-    changedelete = { hl = 'GitGutterChange', text = '~' },
-  },
-}
-
--- Telescope
-require('telescope').setup {
-  defaults = {
-    mappings = {
-      i = {
-        ['<C-u>'] = false,
-        ['<C-d>'] = false,
-      },
-    },
-  },
-}
 
 require("telescope").load_extension "file_browser"
 vim.api.nvim_set_keymap('n', '-', [[:Telescope file_browser path=%:p:h<CR>]], {noremap = true, silent = true })
