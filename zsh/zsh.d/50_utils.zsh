5c3b82d8858c4da51ede78a90c0965401f48ae99
Aljaž Srebrnič <aljaz.srebrnic@u-blox.com>
2022-11-23 13:35:49 +0100

    zsh: fix rust-analyzer gh url

--- zsh/zsh.d/50_utils.zsh
+++ zsh/zsh.d/50_utils.zsh
@@ -2,7 +2,7 @@ zinit wait"2" from"gh-r" as"program" lucid for \
   denisidoro/navi \
   jesseduffield/lazydocker \
   pick'mdcat*/mdcat' lunaryorn/mdcat \
-  mv'rust-analyzer* -> rust-analyzer' rust-analyzer/rust-analyzer \
+  mv'rust-analyzer* -> rust-analyzer' rust-lang/rust-analyzer \
   pick"bin/dog" ogham/dog \
   mv'dry-* -> dry' moncho/dry \
   bcicen/ctop \
