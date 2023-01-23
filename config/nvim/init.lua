63e43703284dc632c3c737e365ec0cbaf9703253
Aljaž Srebrnič <aljaz.srebrnic@u-blox.com>
2023-01-23 14:35:28 +0100

    nvim: use keymap.set instead of using API

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -24,7 +24,8 @@ require("lazy").setup({
   {
       'nvim-telescope/telescope.nvim',
       dependencies = { 'nvim-lua/plenary.nvim' },
-      config = {
+      config = function()
+        require("telescope").setup({
           defaults = {
               mappings = {
                   i = {
@@ -33,9 +34,28 @@ require("lazy").setup({
                   },
               },
           },
-      }
+        })
+
+        vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { silent = true })
+        vim.keymap.set('n', '<leader>ff', function() require('telescope.builtin').find_files{previewer = false} end, { silent = true })
+        vim.keymap.set('n', '<leader>sb', require('telescope.builtin').current_buffer_fuzzy_find, { silent = true })
+        vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { silent = true })
+        vim.keymap.set('n', '<leader>st', require('telescope.builtin').tags, { silent = true })
+        vim.keymap.set('n', '<leader>sd', require('telescope.builtin').grep_string, { silent = true })
+        vim.keymap.set('n', '<leader>sp', require('telescope.builtin').live_grep, { silent = true })
+        vim.keymap.set('n', '<leader>so', function() require('telescope.builtin').tags{ only_current_buffer = true } end, { silent = true })
+        vim.keymap.set('n', '<leader>?',  require('telescope.builtin').oldfiles, { silent = true })
+      end
   },
-  "nvim-telescope/telescope-file-browser.nvim" ,
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { 'nvim-telescope/telescope.nvim' },
+    config = function()
+        require('telescope').load_extension "file_browser"
+
+        vim.keymap.set('n', '-', [[:Telescope file_browser path=%:p:h<CR>]], { silent = true })
+    end
+  },
   'joshdick/onedark.vim', -- Theme inspired by Atom
   'folke/tokyonight.nvim',
   -- Add indentation guides even on blank lines
@@ -57,7 +77,59 @@ require("lazy").setup({
   -- Highlight, edit, and navigate code using a fast incremental parsing library
   {
     'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate'
+    build = ':TSUpdate',
+    config = function()
+        require('nvim-treesitter.configs').setup {
+          highlight = {
+            enable = true, -- false will disable the whole extension
+          },
+          incremental_selection = {
+            enable = true,
+            keymaps = {
+              init_selection = 'gnn',
+              node_incremental = 'grn',
+              scope_incremental = 'grc',
+              node_decremental = 'grm',
+            },
+          },
+          indent = {
+            enable = true,
+          },
+          textobjects = {
+            select = {
+              enable = true,
+              lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+              keymaps = {
+                -- You can use the capture groups defined in textobjects.scm
+                ['af'] = '@function.outer',
+                ['if'] = '@function.inner',
+                ['ac'] = '@class.outer',
+                ['ic'] = '@class.inner',
+              },
+            },
+            move = {
+              enable = true,
+              set_jumps = true, -- whether to set jumps in the jumplist
+              goto_next_start = {
+                [']m'] = '@function.outer',
+                [']]'] = '@class.outer',
+              },
+              goto_next_end = {
+                [']M'] = '@function.outer',
+                [']['] = '@class.outer',
+              },
+              goto_previous_start = {
+                ['[m'] = '@function.outer',
+                ['[['] = '@class.outer',
+              },
+              goto_previous_end = {
+                ['[M'] = '@function.outer',
+                ['[]'] = '@class.outer',
+              },
+            },
+          },
+        }
+    end
   },
   -- Additional textobjects for treesitter
   'nvim-treesitter/nvim-treesitter-textobjects',
@@ -262,13 +334,13 @@ vim.o.expandtab = true
 vim.o.shiftwidth = 4
 
 --Remap space as leader key
-vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent = true })
+vim.keymap.set('', '<Space>', '<Nop>', { silent = true })
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 --Remap for dealing with word wrap
-vim.api.nvim_set_keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
-vim.api.nvim_set_keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true })
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Highlight on yank
 vim.cmd [[
@@ -279,7 +351,7 @@ vim.cmd [[
 ]]
 
 -- Y yank until the end of line  (note: this is now a default on master)
-vim.api.nvim_set_keymap('n', 'Y', 'y$', { noremap = true })
+vim.keymap.set('n', 'Y', 'y$')
 
 --Map blankline
 vim.g.indent_blankline_char = '┊'
@@ -288,75 +360,7 @@ vim.g.indent_blankline_buftype_exclude = { 'terminal', 'nofile' }
 vim.g.indent_blankline_char_highlight = 'LineNr'
 vim.g.indent_blankline_show_trailing_blankline_indent = false
 
-require("telescope").load_extension "file_browser"
-vim.api.nvim_set_keymap('n', '-', [[:Telescope file_browser path=%:p:h<CR>]], {noremap = true, silent = true })
-
---Add leader shortcuts
-vim.api.nvim_set_keymap('n', '<leader><space>', [[<cmd>lua require('telescope.builtin').buffers()<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>ff', [[<cmd>lua require('telescope.builtin').find_files({previewer = false})<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>sb', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>sh', [[<cmd>lua require('telescope.builtin').help_tags()<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>st', [[<cmd>lua require('telescope.builtin').tags()<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>sd', [[<cmd>lua require('telescope.builtin').grep_string()<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>sp', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>so', [[<cmd>lua require('telescope.builtin').tags{ only_current_buffer = true }<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>?', [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>fs', [[<cmd>:w<CR>]], { noremap = true })
-
-
-
--- Treesitter configuration
--- Parsers must be installed manually via :TSInstall
-require('nvim-treesitter.configs').setup {
-  highlight = {
-    enable = true, -- false will disable the whole extension
-  },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = 'gnn',
-      node_incremental = 'grn',
-      scope_incremental = 'grc',
-      node_decremental = 'grm',
-    },
-  },
-  indent = {
-    enable = true,
-  },
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
-      },
-    },
-    move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = {
-        [']m'] = '@function.outer',
-        [']]'] = '@class.outer',
-      },
-      goto_next_end = {
-        [']M'] = '@function.outer',
-        [']['] = '@class.outer',
-      },
-      goto_previous_start = {
-        ['[m'] = '@function.outer',
-        ['[['] = '@class.outer',
-      },
-      goto_previous_end = {
-        ['[M'] = '@function.outer',
-        ['[]'] = '@class.outer',
-      },
-    },
-  },
-}
+vim.keymap.set('n', '<leader>fs', "<cmd>:w<CR>")
 
 -- LSP settings
 local nvim_lsp = require 'lspconfig'
@@ -414,11 +418,10 @@ vim.g.committia_hooks = {
   end
 }
 
-vim.api.nvim_set_keymap("n", "]q", "<cmd>:cnext<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "[q", "<cmd>:cprev<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "]b", "<cmd>:bnext<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "[b", "<cmd>:bprev<CR>", {noremap = true, silent = true})
-
+vim.keymap.set("n", "]q", "<cmd>:cnext<CR>", { silent = true})
+vim.keymap.set("n", "[q", "<cmd>:cprev<CR>", { silent = true})
+vim.keymap.set("n", "]b", "<cmd>:bnext<CR>", { silent = true})
+vim.keymap.set("n", "[b", "<cmd>:bprev<CR>", { silent = true})
 
 -- Disable virtual_text since it's redundant due to lsp_lines.
 vim.diagnostic.config({
