43477cea52600847da08135c891c3c41b3b9e048
Aljaž Srebrnič <aljaz.srebrnic@u-blox.com>
2023-01-31 16:06:54 +0100

    zsh: remove zpy, I should use pdm

--- zsh/zsh.d/50_plugin.zsh
+++ zsh/zsh.d/50_plugin.zsh
@@ -1,6 +1,5 @@
 zinit lucid for \
     Tarrasch/zsh-bd \
-    andydecleyre/zpy \
     b4b4r07/emoji-cli \
     zdharma-continuum/fast-syntax-highlighting \
     zdharma-continuum/history-search-multi-word
