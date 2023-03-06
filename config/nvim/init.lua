8482cad1010b54fc08c7844e3b673a2453a603d7
Aljaž Srebrnič <aljaz.srebrnic@u-blox.com>
2023-03-06 13:58:24 +0100

    vim: add rust-tools

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -425,6 +425,19 @@ require("lazy").setup({
     '/IndianBoy42/tree-sitter-just',
     config = true,
   },
+  {
+    'simrat39/rust-tools.nvim',
+    config = {
+      server = {
+        on_attach = function(_, bufnr)
+                -- Hover actions
+          vim.keymap.set("n", "<C-space>", require("rust-tools").hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+          vim.keymap.set("n", "<Leader>a", require("rust-tools").code_action_group.code_action_group, { buffer = bufnr })
+        end
+      }
+    },
+  },
 })
 
 --Set highlight on search
