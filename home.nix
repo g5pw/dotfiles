038e9f50bfc4371f60e345c70403d5bf3a9367b5
Aljaž "g5pw" Srebrnič <me@g5pw.me>
2024-03-15 11:24:10 +0100

    nix: remove test var

--- home.nix
+++ home.nix
@@ -184,7 +184,6 @@
   # if you don't want to manage your shell through Home Manager.
   home.sessionVariables = {
     EDITOR = "nvim";
-    TOAST = "test";
   };
 
   # Let Home Manager install and manage itself.
