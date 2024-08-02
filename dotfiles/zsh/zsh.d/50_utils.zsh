cb2b225316080afa1e3f09903ced88ad44bdb79b
Aljaž "g5pw" Srebrnič <me@g5pw.me>
2024-08-02 18:07:25 +0200

    zsh: remove irust dep

--- dotfiles/zsh/zsh.d/50_utils.zsh
+++ dotfiles/zsh/zsh.d/50_utils.zsh
@@ -2,5 +2,4 @@ zinit wait"2" from"gh-r" as"program" lucid for \
   bpick'*darwin-amd64*' mv'dry* -> dry' moncho/dry \
   mv'spacedisplay-* -> spacedisplay' funbiscuit/spacedisplay-rs \
   mv'punycode_* -> punycode' jonasbn/punycode \
-  mv'irust-* -> irust' sigmaSd/IRust \
   mrusme/neonmodem
