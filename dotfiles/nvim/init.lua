f005e8a7d5a6a805dd25ddd795a567cea5e3f01a
Aljaž "g5pw" Srebrnič <me@g5pw.me>
2025-01-31 23:37:35 +0100

    nvim: update dashboard config

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -20,44 +20,43 @@ require("lazy").setup({
 	{
 		"nvimdev/dashboard-nvim",
 		event = "VimEnter",
-		config = function()
-			require("dashboard").setup({
-				config = {
-					week_header = {
-						enable = false,
-					},
-					shortcut = {
-						{
-							desc = "󰊳 Update",
-							group = "@property",
-							action = "Lazy update",
-							key = "u",
-						},
-						{
-							icon = " ",
-							icon_hl = "@variable",
-							desc = "Files",
-							group = "Label",
-							action = "Telescope find_files",
-							key = "f",
-						},
-						{
-							desc = " Apps",
-							group = "DiagnosticHint",
-							action = "Telescope app",
-							key = "a",
-						},
-						{
-							desc = " dotfiles",
-							group = "Number",
-							action = "Telescope dotfiles",
-							key = "d",
-						},
-					},
-				},
-			})
-		end,
 		dependencies = { { "nvim-tree/nvim-web-devicons" } },
+                config = {
+                    theme = 'hyper',
+                    config = {
+                      week_header = {
+                       enable = false,
+                      },
+                      shortcut = {
+                        { 
+                          desc = '󰊳 Update',
+                          group = '@property',
+                          action = 'Lazy update',
+                          key = 'u',
+                        },
+                        {
+                          icon = ' ',
+                          icon_hl = '@variable',
+                          desc = 'Files',
+                          group = 'Label',
+                          action = 'Telescope find_files',
+                          key = 'f',
+                        },
+                        {
+                          desc = '󰞋 Help',
+                          group = 'DiagnosticHint',
+                          action = 'Telescope help_tags',
+                          key = 'h',
+                        },
+                        {
+                          desc = ' dotfiles',
+                          group = 'Number',
+                          action = 'Telescope find_files cwd=~/.config/nix-darwin/dotfiles/',
+                          key = 'd',
+                        },
+                      },
+                    },
+                  },
 	},
 	{
 		"liangxianzhe/nap.nvim",
