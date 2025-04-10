daa3aba2786567219082c838c65a6d3a79c2a846
Aljaž "g5pw" Srebrnič <me@g5pw.me>
2025-04-10 11:29:59 +0200

    vim: Added cmake language server

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -627,4 +627,4 @@ vim.g.rustaceanvim = {
     },
 }
 
-vim.lsp.enable({'luals', 'clangd'})
+vim.lsp.enable({'luals', 'clangd', 'cmake-language-server'})
---
+++ dotfiles/nvim/lsp/cmake-language-server.lua
@@ -0,0 +1,5 @@
+return {
+  cmd = { 'cmake-language-server' },
+  root_markers = { 'CMakeLists.txt' },
+  filetypes = { 'CMakeLists.txt', 'cmake' },
+}
