5f436c028c33fae51c3df4713d07fff28f3427f7
Aljaž "g5pw" Srebrnič <me@g5pw.me>
2025-04-30 12:06:39 +0200

    nvim: add enter to accept suggestion

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -303,13 +303,12 @@ require("lazy").setup({
                         -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
                         -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
                         -- See the full "keymap" documentation for information on defining your own keymap.
-                        keymap = { preset = 'super-tab' },
+                        keymap = {
+                            preset = 'super-tab',
+                            ['<CR>'] = { 'accept', 'fallback' },
+                        },
 
                         appearance = {
-                            -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-                            -- Useful for when your theme doesn't support blink.cmp
-                            -- Will be removed in a future release
-                            use_nvim_cmp_as_default = true,
                             -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
                             -- Adjusts spacing to ensure icons are aligned
                             nerd_font_variant = 'mono'
@@ -327,7 +326,6 @@ require("lazy").setup({
                                 },
                         },
                 },
-                opts_extend = { "sources.default" }
         },
 	{
 		"stevearc/aerial.nvim",
