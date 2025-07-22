4c1f9426338c3a7d1a42dec3312863973ea46ffa
Aljaž "g5pw" Srebrnič <me@g5pw.me>
2025-07-22 11:42:10 +0200

    nvim: reorder main config file in sections

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -1,7 +1,4 @@
--- vim:
-
-vim.g.mapleader = " "
-
+-- Options {{{
 --Set highlight on search
 vim.o.hlsearch = true
 
@@ -31,16 +28,26 @@ vim.o.termguicolors = true
 
 vim.o.expandtab = true
 vim.o.shiftwidth = 4
+vim.o.softtabstop = -1
 
 --Remap space as leader key
 vim.keymap.set("", "<Space>", "<Nop>", { silent = true })
 vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+vim.g.maplocalleader = ","
 
---Remap for dealing with word wrap
-vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = "menuone,popup,noinsert"
+vim.o.wildmode = "list:longest"
 
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.o.foldtext = "v:lua.vim.treesitter.foldtext()"
+vim.o.foldlevelstart = 99
+
+vim.o.guifont = "MonaspiceAr Nerd Font:h10"
+-- }}}
+
+-- Autocmds {{{
 -- Highlight on yank
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
   desc = "Briefly highlight yanked text",
@@ -56,9 +63,12 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     vim.api.nvim_set_keymap("n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
   end,
 })
+-- }}}
 
--- Set completeopt to have a better completion experience
-vim.o.completeopt = "menuone,noselect"
+-- Keymaps {{{
+--Remap for dealing with word wrap
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 vim.keymap.set("n", "<leader>fs", "<cmd>:w<CR>")
 
@@ -66,6 +76,9 @@ vim.keymap.set("n", "<leader>w/", vim.cmd.vsplit)
 vim.keymap.set("n", "<leader>w-", vim.cmd.split)
 
 vim.keymap.set("n", "<leader>bd", vim.cmd.bdelete, { desc = "Delete current buffer" })
+
+vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, {desc = 'LSP Rename'})
+-- }}}
 
 ---@type rustaceanvim.Opts
 vim.g.rustaceanvim = {
@@ -97,15 +110,7 @@ vim.g.rustaceanvim = {
   },
 }
 
-vim.o.foldmethod = "expr"
-vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-vim.o.foldtext = "v:lua.vim.treesitter.foldtext()"
-vim.o.foldlevelstart = 99
-
 vim.lsp.enable({ "luals", "clangd", "cmake-language-server", "tinymist" })
-vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, {desc = 'LSP Rename'})
-
-vim.o.guifont = "MonaspiceAr Nerd Font:h10"
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
