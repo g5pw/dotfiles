0601c4397c50ba49fb4b090bb440ec023ce65937
Aljaž "g5pw" Srebrnič <me@g5pw.me>
2025-07-30 11:20:25 +0200

    nvim: lazy load conform.nvim

--- dotfiles/nvim/lua/plugins/init.lua
+++ dotfiles/nvim/lua/plugins/init.lua
@@ -342,6 +342,13 @@ return {
   },
   {
     "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+    { "<leader>cf", function() require("conform").format({ async = true }) end, mode = "", desc = "Format buffer", },
+    },
+    ---@module "conform"
+    ---@type conform.setupOpts
     opts = {
       formatters_by_ft = {
         nix = { "nixfmt" },
